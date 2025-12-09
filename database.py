d7d06f1271f91313ea3a0988d744e7c31aed49aa
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-09 18:03:14 +0100

    Initialized Database Connection

---
+++ database.py
@@ -0,0 +1,12 @@
+import psycopg2
+from sqlalchemy import create_engine
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL)
+
+connection = engine.connect()
