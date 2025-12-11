ace25475a2ef3cec9fcddad3b15427930f22e48e
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-11 12:58:11 +0100

    Adding foreign keys

--- app/models.py
+++ app/models.py
@@ -6,12 +6,13 @@ Base = declarative_base()
 
 # defining models for db tables
 class User(Base):
+    __tablename__ = 'user'
+
     userId = Column(Integer, autoincrement=True, unique=True, primary_key=True)
     userName = Column(String(15), unique=True, nullable=False)
     userEmail = Column(String(100), unique=True, nullable=False)
     passwordHash = Column(String(255), nullable=False)
     dateCreated = Column(DateTime, nullable=False)
-    # rel_to_msgs = Column(Integer, ForeignKey('messageId', ondelete='CASCADE'), nullable=False)
 
 
     firstName = Column(String(50), nullable=False)
@@ -19,15 +20,25 @@ class User(Base):
 
 
 class Message(Base):
-    messageId = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    senderId = Column(Integer, nullable=False, unique=True)
+    __tablename__ = 'message'
+
+    messageId = Column(Integer, autoincrement=True, nullable=False, primary_key=True) 
+
+    # retrieve sender and recipient via userId, 
+    senderId = Column(Integer, ForeignKey('userId'), nullable=False) 
     recipientId = Column(Integer, nullable=False, unique=True)
-    rel_to_conversation = Column(Integer, ForeignKey('conversationId', ondelete='CASCADE'), nullable=False)
+
+    # retrieve conversation (of which message is a part of) via conversationId
+    conversationId = Column(Integer, ForeignKey('conversationId', ondelete='CASCADE'), nullable=False)
     
-    messageTxt  = Column(String(250), nullable=False)
+    messageText  = Column(String(250), nullable=False)
     timeStamp = Column(DateTime, nullable=False)
     
 class Conversation(Base):
+    __tablename__ = 'conversation'
+
     conversationId = Column(Integer, nullable=False, unique=True, primary_key=True)
-    user1_Id = Column(Integer, nullable=False, unique=True)
-    user2_Id = Column(Integer, nullable=False, unique=True)
+
+    # retrieve conversation participants via userId
+    user1_Id = Column(Integer, ForeignKey('userId'), nullable=False, unique=True)
+    user2_Id = Column(Integer, ForeignKey('userId'), nullable=False, unique=True)
