b01fa64696f40eb39f31041b1f584e31334a109d
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-15 23:46:57 +0100

    Wired routes and services

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,8 +1,37 @@
 from fastapi import APIRouter
+from schemas import createUser, readUser
+from services.user_ops import *
+from utils.hashing import *
 
 router = APIRouter()
 
 
-@router.get("/test")
-def signup():
-    return {"Hi, I am a user!"}
+@router.post("/signup", response_model=readUser)
+def signup(user: createUser) -> User:
+    new_user: readUser = create_user(
+        firstName=user.firstName,
+        lastName=user.lastName,
+        userName=user.userName,
+        userEmail=user.userEmail,
+        passwordHash=hash_password(user.password),
+        dateCreated=datetime.now(),
+    )
+    # return newly added user
+    return get_user_by_userName(user.userName)
+
+
+@router.post("/login", response_model=readUser)
+def login(userName, password) -> User:
+    if verify_password(userName, hash_password(password)):
+        return get_user_by_userName(userName)
+    else:
+        return "Invalid Credentials"
+    
+@router.get("me", response_model=readUser)
+def current_user(userName):
+    return get_user_by_userName(userName)
+
+
+@router.get('{users_id}', response_model=readUser)
+def get_user_profile(userId: Uuid):
+    return get_user_by_Id(userId)
