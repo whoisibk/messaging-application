e5c7683b2923cd0efd3f31d872690c10712a5ec6
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-17 23:51:29 +0100

    Better documentation

--- app/services/user_ops.py
+++ app/services/user_ops.py
@@ -3,14 +3,13 @@ from database import session
 from datetime import datetime
 
 
-# from utils.hashing import hash_password, verify_password
+"""    Service functions for user operations    """
 
 
 def create_user(
     userName, userEmail, passwordHash: str, firstName, lastName, dateCreated: datetime
 ) -> User:
-
-    # Create a new user and add to the database
+    """# Create a new user and add to the database"""
     new_user: User = User(
         userName=userName,
         userEmail=userEmail,
@@ -29,21 +28,22 @@ def create_user(
 
 
 def get_user_by_userName(userName) -> User:
-    # Retrieve a user from the database by userName
+    """ Retrieve a user from the database by userName"""
+
     db_session = session
     user: User = db_session.query(User).filter(User.userName == userName).first()
     return user if user else None
 
 
 def get_user_by_Id(userId: Uuid) -> User:
-    # Retrieve a user from the database by userId
+    """Retrieve a user from the database by userId"""
     db_session = session
     user: User = db_session.query(User).filter(User.userId == userId).first()
     return user if user else None
 
 
 def verify_user_credentials(userName, passwordHash: str) -> bool:
-    # Verify user credentials
+    """# Verify user credentials"""
     user: User = get_user_by_userName(userName)
     if user.passwordHash == passwordHash:
         return True
