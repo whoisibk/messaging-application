8f635746fcd48387dee2e9040bfe43020f21ffa0
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-18 15:55:23 +0100

    Fixing function signature for sending a message

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -1,8 +1,52 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
+
+from services.message_ops import create_message
+from schemas import sentMessage, createMessage
+from services.message_ops import *
+from services.conversation_ops import *
+from utils.auth import create_jwt_token
+from users import get_current_user, User, Uuid
 
 router = APIRouter()
 
 
-@router.get("/test")
-def message():
-    return {"This is a message!"}
+@router.post("/send_message", response_model=sentMessage)
+def send_message(
+    recipientId: Uuid, messageText: str, sender: User = Depends(get_current_user)
+) -> sentMessage:
+    """
+    Endpoint for sending a message from one user to another
+    Args:
+        recipientId (Uuid): ID of the message's recipient
+        messageText(str)): message text content
+        sender (User): User model of the message's sender
+    """
+
+    """extracts convoId if not none, else create and then extract"""
+    get_conv = get_conversation_between_users()
+    if get_conv is None:
+        conversationId = create_conversation(
+            user1_Id=sender.userId, user2_id=recipientId
+        ).conversationId
+    else:
+        conversationId = get_conv.conversationId
+
+    """ saves message to the database so it can be fetched later """
+    message_info = create_message(
+        recipientId=recipientId,
+        messageText=messageText,
+        senderId=sender.userId,
+        timestamp=datetime.now(),
+        conversationId=conversationId,
+    )
+
+    return {
+        "status": 200,
+        "conversationId": message_info.conversationId,
+        "timestamp": message_info.timestamp,
+        "message": message_info.messageText,
+        "sent by": message_info.senderId,
+    }
+
+    # realtime convo will be integrated later during websockets
