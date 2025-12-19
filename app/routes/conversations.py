0562001a48fb22afb21f50af53e23b98e6f17009
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-19 17:36:50 +0100

    Wrote endpoints for conversations route

--- app/routes/conversations.py
+++ app/routes/conversations.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from typing import List
+from services.conversation_ops import conversations_for_user, Conversation
+from users import get_current_user, User
 
 router = APIRouter()
 
 
 @router.get("/test")
-def conversation():
-    return {"This is a convo!"}
+def get_conversation(user: User = Depends(get_current_user)) -> List[Conversation]:
+    """Retrieve all conversations a user has participated in"""
+
+    conversations = conversations_for_user(user1_id=user.userId)
+    if conversations is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No conversations were found",
+        )
+    return conversations
