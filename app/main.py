5e5c4ab43ea1eb6c9b7d6ed36bdaa215893d1a77
whoisibk <oyetimehinjoseph@gmail.com>
2026-01-01 17:16:37 +0100

    Added websocket route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from uvicorn import run
 from typing import Optional
 from enum import Enum
 from pydantic import BaseModel, Field
@@ -7,16 +8,23 @@ from routes.users import router as users_route
 from routes.messages import router as messages_route
 from routes.conversations import router as conversations_route
 
-import database
+from websocket_endpoint import wb_route as websocket_route
 
 
 app = FastAPI()
+
 
 app.include_router(users_route, prefix="/users")
 app.include_router(messages_route, prefix="/messages")
 app.include_router(conversations_route, prefix="/conversations")
 
+app.include_router(websocket_route, prefix="/ws")
+
 
 @app.get("/")
 def test():
     return {"message": "Hello World!"}
+
+
+if __name__ == "__main__":
+    run("app.main:app", host="127.0.0.1", port=8000, reload=True)
