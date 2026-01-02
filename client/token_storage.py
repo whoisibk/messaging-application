d37f4aa9436a27ec3b060ae8bbc623bef2b2a08c
whoisibk <oyetimehinjoseph@gmail.com>
2026-01-02 23:49:16 +0100

    Handled client's storage of tokens

---
+++ client/token_storage.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import json
+
+
+# File path (to exist in current working directory) to store the token data for the client 
+TOKEN_PATH = Path.cwd() / "mytoken.json" 
+
+def save_token(token_data: dict):
+    """Saves the authentication token data to a file in JSON format."""    
+    TOKEN_PATH.write_text(json.dumps(token_data))
+
+
+def load_token()-> dict | None:
+    """Retrieve stored token data"""
+
+    if TOKEN_PATH.exists() is None:
+        return None
+    return json.loads(TOKEN_PATH.read_text())
